8bc67f671215c99c84d7fc3c0add3001d4f57538
anergcorp <ahunatajanov@gmail.com>
2024-03-30 19:40:30 +0200

    creating function screenshot

--- src/browser/browser.py
+++ src/browser/browser.py
@@ -1,4 +1,8 @@
+import os
 from playwright.sync_api import sync_playwright, TimeoutError
+
+from src.config import Config
+from src.state import AgentState
 
 class Browser:
     def __init__(self):
@@ -17,3 +21,23 @@ class Browser:
             print(f"TimeoutError: {e} when trying to navigate to {url}")
             return False
         return True
+    
+    def screenshot(self, project_name):
+        screenshots_save_path = Config().get_screenshots_dir()
+
+        page_metadata = self.page.evaluate("() => { return { url: document.location.href, title: document.title } }")
+        page_url = page_metadata['url']
+        random_filename = os.urandom(20).hex()
+        filename_to_save = f"{random_filename}.png"
+        path_to_save = os.path.join(screenshots_save_path, filename_to_save)
+
+        self.page.emulate_media(media="screen")
+        self.page.screenshot(path=path_to_save)
+
+        new_state = AgentState().new_state()
+        new_state["internal_monologue"] = "Browsing the web right now..."
+        new_state["browser_session"]["url"] = page_url
+        new_state["browser_session"]["screenshot"] = path_to_save
+        AgentState().add_to_current_state(project_name, new_state)        
+
+        return path_to_save
